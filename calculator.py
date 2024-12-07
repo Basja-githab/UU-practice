d72265118637c5703aae03c26f0d23b79cd64611
Basja-githab <basja035@gmail.com>
2024-12-07 15:53:01 +0400

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -20,18 +20,4 @@ def calc():
         print(f"Результат вычисления: {result}")
     except Exception as error:
         print("Введены некорректные данные. Ошибка:", error)
-#calc()
-print(range(3))
-dict = {}
-dict["name"] = 'bhf'
-dict.pop("name")
-print(dict)
-#python
-my_dict = {"a": 1, "b": 2}
-print(my_dict.get("a"))
-my_set = {1,2,3}
-my_set.add(4)
-print(my_set)
-def my_function(x):
-    return x + 1
-print(my_function(5))
+calc()
